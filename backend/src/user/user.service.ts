b56e61d21bdfeeebae2664e912d0b52a7a2d2e4f
Milos Dimitrijevic <milos.dimitrijevic@elfak.rs>
2026-06-04 14:47:39 +0200

    Hash user passwords with bcrypt

--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-
+import * as bcrypt from 'bcrypt';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { User } from './entities/user.entity';
@@ -13,16 +13,18 @@ export class UserService {
     private readonly userRepository: Repository<User>,
   ) {}
 
-  create(createUserDto: CreateUserDto) {
-    const user = this.userRepository.create({
-      username: createUserDto.username,
-      email: createUserDto.email,
-      password: createUserDto.password,
-      role: createUserDto.role ?? 'USER',
-    });
+  async create(createUserDto: CreateUserDto) {
+  const hashedPassword = await bcrypt.hash(createUserDto.password, 10);
 
-    return this.userRepository.save(user);
-  }
+  const user = this.userRepository.create({
+    username: createUserDto.username,
+    email: createUserDto.email,
+    password: hashedPassword,
+    role: createUserDto.role ?? 'USER',
+  });
+
+  return this.userRepository.save(user);
+}
 
   findAll() {
     return this.userRepository.find();
@@ -47,12 +49,16 @@ export class UserService {
   }
 
   async update(id: number, updateUserDto: UpdateUserDto) {
-    const user = await this.findOne(id);
+  const user = await this.findOne(id);
 
-    Object.assign(user, updateUserDto);
-
-    return this.userRepository.save(user);
+  if (updateUserDto.password) {
+    updateUserDto.password = await bcrypt.hash(updateUserDto.password, 10);
   }
+
+  Object.assign(user, updateUserDto);
+
+  return this.userRepository.save(user);
+}
 
   async remove(id: number) {
     const user = await this.findOne(id);
