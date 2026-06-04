065193de36d7fb0aa7b5271f8a655cdc47a250db
Milos Dimitrijevic <milos.dimitrijevic@elfak.rs>
2026-06-04 14:55:33 +0200

    Add auth login with password validation

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,7 +1,12 @@
-import { Injectable, ConflictException } from '@nestjs/common';
+import {
+  Injectable,
+  ConflictException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { UserService } from '../user/user.service';
 import { RegisterDto } from './dto/register.dto';
-
+import * as bcrypt from 'bcrypt';
+import { LoginDto } from './dto/login.dto';
 @Injectable()
 export class AuthService {
   constructor(private readonly userService: UserService) {}
@@ -30,4 +35,30 @@ export class AuthService {
       },
     };
   }
+  async login(loginDto: LoginDto) {
+  const user = await this.userService.findByEmail(loginDto.email);
+
+  if (!user) {
+    throw new UnauthorizedException('Invalid email or password');
+  }
+
+  const passwordMatches = await bcrypt.compare(
+    loginDto.password,
+    user.password,
+  );
+
+  if (!passwordMatches) {
+    throw new UnauthorizedException('Invalid email or password');
+  }
+
+  return {
+    message: 'Login successful',
+    user: {
+      id: user.id,
+      username: user.username,
+      email: user.email,
+      role: user.role,
+    },
+  };
+}
 }
