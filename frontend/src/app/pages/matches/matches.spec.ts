9dc2649adb6bf228254f10155df368d843631b8e
Milos Dimitrijevic <milosdimitrijevicc2005@gmail.com>
2026-09-14 15:16:52 +0200

    test(frontend): configure router directives in page tests

--- frontend/src/app/pages/matches/matches.spec.ts
+++ frontend/src/app/pages/matches/matches.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { Matches } from './matches';
 
@@ -9,6 +10,7 @@ describe('Matches', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [Matches],
+      imports: [RouterTestingModule],
     }).compileComponents();
 
     fixture = TestBed.createComponent(Matches);
